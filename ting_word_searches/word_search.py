393f9d60f462731e41391f5e2211ec3ecc10e5ab
Italo Araujo <italojsouza@gmail.com>
2024-02-04 19:09:36 -0300

    feat: word_searches

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,38 @@
-def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+from ting_file_management.queue import Queue
+
+
+def exists_word(word, instance: Queue):
+    result = []
+    for item in instance._data:
+        information = {
+            "palavra": word,
+            "arquivo": item["nome_do_arquivo"],
+            "ocorrencias": [],
+        }
+        for index in range(len(item["linhas_do_arquivo"])):
+            if word.lower() in item["linhas_do_arquivo"][index].lower():
+                information["ocorrencias"].append({"linha": index + 1})
+        if len(information["ocorrencias"]) > 0:
+            result.append(information)
+    return result
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+    for item in instance._data:
+        information = {
+            "palavra": word,
+            "arquivo": item["nome_do_arquivo"],
+            "ocorrencias": [],
+        }
+        for index in range(len(item["linhas_do_arquivo"])):
+            if word.lower() in item["linhas_do_arquivo"][index].lower():
+                information["ocorrencias"].append(
+                    {
+                        "linha": index + 1,
+                        "conteudo": item["linhas_do_arquivo"][index],
+                    }
+                )
+        if len(information["ocorrencias"]) > 0:
+            result.append(information)
+    return result

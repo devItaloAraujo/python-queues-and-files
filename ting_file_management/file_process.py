ba677323ba7aa0dccbaacecb8bbcb331b658508b
Italo Araujo <italojsouza@gmail.com>
2024-02-04 17:58:06 -0300

    feat: file_process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,10 +1,34 @@
-def process(path_file, instance):
-    """Aqui irá sua implementação"""
+from ting_file_management.queue import Queue
+from ting_file_management.file_management import txt_importer
+import sys
 
 
-def remove(instance):
-    """Aqui irá sua implementação"""
+def process(path_file, instance: Queue):
+    linhas = txt_importer(path_file)
+
+    value = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(linhas),
+        "linhas_do_arquivo": linhas,
+    }
+    if value not in instance._data:
+        instance.enqueue(value)
+        print(value, file=sys.stdout)
+
+
+def remove(instance: Queue):
+    if instance.is_empty():
+        return print("Não há elementos", file=sys.stdout)
+    removido = instance.dequeue()
+    print(
+        f"Arquivo {removido['nome_do_arquivo']} removido com sucesso",
+        file=sys.stdout,
+    )
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    try:
+        result = instance.search(position)
+        print(result, file=sys.stdout)
+    except IndexError:
+        print("Posição inválida", file=sys.stderr)

79c9e95a40241aaff04530510f8fdeb887f8e476
Italo Araujo <italojsouza@gmail.com>
2024-02-04 17:17:36 -0300

    feat: txt_importer

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,15 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+
+    if not path_file.endswith(".txt"):
+        return print("Formato inválido", file=sys.stderr)
+
+    # raise error if file does not exist
+    try:
+        with open(path_file, "r") as file:
+            result = file.read().split("\n")
+            return result
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
